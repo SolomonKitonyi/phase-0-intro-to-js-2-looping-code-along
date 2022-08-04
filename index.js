4b26d2cab26890d14c9186c9407b086859143445
SolomonKitonyi <kitonyisolomon@gmail.com>
2022-08-04 08:44:11 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+const names = ["Ada", "Brendan", "Ali"];
+let newArray = [];
+
+function writeCards (arr, event) {
+    for(let count=0;count < arr.length;count++) {
+        newArray.push(`Thank you, ${arr[count]}, for the wonderful ${event} gift!`)
+    }
+    return newArray;
+}
+function countDown(num) {
+    for(let count=num; count >= 0; count--){
+        console.log(count);
+    }
+}
